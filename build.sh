baca1bfce42fe7f2a94897104e50149fe6a36cf8
Thomas Edvalson <machin3@gmail.com>
2015-12-03 02:37:26 -0500

    Build nihstro statically

--- build.sh
+++ build.sh
@@ -4,21 +4,18 @@ set -ex
 export CC=gcc-4.9
 export CXX=g++-4.9
 
-export OUTDIR=${HOME}/3ds-tools
-mkdir $OUTDIR
-
 # Build makerom (commit 80d3d99)
 git clone https://github.com/profi200/Project_CTR.git
-cd Project_CTR/makerom && git reset --hard 80d3d99 && make -j4 && cp makerom $OUTDIR && cd -
+cd Project_CTR/makerom && git reset --hard 80d3d99 && make -j4 && cd -
 
 # Build latest 3dsxtool
 git clone https://github.com/devkitPro/3dstools.git
-cd 3dstools && ./autogen.sh && ./configure && make -j4 && cp 3dsxtool $OUTDIR && cd -
+cd 3dstools && ./autogen.sh && ./configure && make -j4 && cd -
 
 # Build latest bannertool
 git clone https://github.com/Steveice10/bannertool.git
-cd bannertool && cmake . && make -j4 && cp bannertool $OUTDIR && cd -
+cd bannertool && cmake . && make -j4 && cd -
 
 # Build nihstro
 git clone https://github.com/neobrain/nihstro.git
-cd nihstro && cmake . && make -j4 && cp nihstro-assemble $OUTDIR && cd -
+cd nihstro && cmake -DBoost_USE_STATIC_LIBS=ON . && make -j4 && cd -
--- deploy-prepare.sh
+++ deploy-prepare.sh
@@ -2,5 +2,13 @@
 set -ex
 
 export RELEASE_FILENAME=3ds-tools-$TRAVIS_OS_NAME-$TRAVIS_TAG.tar.xz
+OUTDIR=${HOME}/3ds-tools
+mkdir $OUTDIR
+
+cp Project_CTR/makerom/makerom $OUTDIR
+cp 3dstools/3dsxtool $OUTDIR
+cp bannertool/bannertool $OUTDIR
+cp nihstro/nihstro-assemble $OUTDIR
+
 cd ${HOME}
 tar -cJvf $RELEASE_FILENAME 3ds-tools
